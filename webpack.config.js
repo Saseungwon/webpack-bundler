cf82b881dbe1b25e1c3bfab7ba3eca37a27dbdf8
dfg1425@naver.com <dfg1425@naver.com>
2022-05-20 19:48:12 +0900

    postscssrc

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,23 @@ module.exports = {
     clean: true   // 기존에 있던 파일을 삭제해줌
   },
 
+  module: {
+    rules: [
+      { 
+        test: /\.s?css$/,
+        use: [ 
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      { 
+        test: /\.js$/,
+        use: [ 'babel-loader']
+      }
+    ]
+  },
 
   // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
   plugins: [
